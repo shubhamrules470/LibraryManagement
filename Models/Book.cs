58147e35bf93dd4caca67c79b6c289e3b28a8b11
shubham.k@technovert.net <shubham.k@technovert.net>
2021-01-19 11:27:43 +0530

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public string Subject { get; set; }
+        public Language Language { get; set; }
+        public string AuthorName { get; set; }
+        public string PublisherName { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public DateTime DateOfPurchase { get; set; }
+        public int NoOfPages { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Models/enums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Models
+{
+    public enum Language
+    {
+        English = 0,
+        Hindi,
+        Telugu,
+        Tamil,
+        Urdu,
+        Bengali,
+        Chinese,
+        Russian,
+        French,
+        Arabic
+    }
+
+    public enum AccountStatus
+    {
+        Active = 0,
+        Blocked
+    }
+}
